a54c50666e2d9b0c757be2a3303b1c7173104c69
alvinlal <alvinzzz2001@gmail.com>
2022-09-07 00:03:19 +0530

    after adding xstate

--- src/components/VideoCard/VideoCard.machine.ts
+++ src/components/VideoCard/VideoCard.machine.ts
@@ -1,1 +1,116 @@
-import { createMachine } from 'xstate';
+import { assign, createMachine } from 'xstate';
+
+export interface netflixStyleVideHoverContext {
+  hasVideoLoaded: boolean;
+}
+
+export type netflixStyleVideHoverEvents =
+  | {
+      type: 'MOUSE_OVER';
+    }
+  | {
+      type: 'MOUSE_OUT';
+    }
+  | {
+      type: 'REPORT_VIDEO_LOADED';
+    };
+
+export const netflixStyleVideoHoverMachine = createMachine<
+  netflixStyleVideHoverContext,
+  netflixStyleVideHoverEvents
+>(
+  {
+    predictableActionArguments: true,
+    id: 'netflixStyleVideoHoverMachine',
+    initial: 'idle',
+    context: {
+      hasVideoLoaded: false,
+    },
+    states: {
+      idle: {
+        on: {
+          MOUSE_OVER: {
+            target: 'showingVideo',
+          },
+          REPORT_VIDEO_LOADED: {
+            actions: 'reportVideoLoaded',
+          },
+        },
+      },
+      showingVideo: {
+        initial: 'checkingIfVideoHasLoaded',
+        on: {
+          MOUSE_OUT: {
+            target: 'idle',
+          },
+        },
+        states: {
+          checkingIfVideoHasLoaded: {
+            always: [
+              {
+                cond: 'hasLoadedVideo',
+                target: 'waitingBeforePlaying',
+              },
+              {
+                target: 'loadingVideoSrc',
+              },
+            ],
+          },
+          waitingBeforePlaying: {
+            after: {
+              2000: {
+                target: 'autoPlayingVideo',
+              },
+            },
+          },
+          loadingVideoSrc: {
+            initial: 'cannotMoveOn',
+            onDone: {
+              target: 'autoPlayingVideo',
+            },
+            states: {
+              cannotMoveOn: {
+                after: {
+                  2000: {
+                    target: 'canMoveOn',
+                  },
+                },
+                on: {
+                  REPORT_VIDEO_LOADED: {
+                    actions: 'reportVideoLoaded',
+                  },
+                },
+              },
+              canMoveOn: {
+                always: {
+                  cond: 'hasLoadedVideo',
+                  target: 'loaded',
+                },
+                on: {
+                  REPORT_VIDEO_LOADED: {
+                    actions: 'reportVideoLoaded',
+                    target: 'loaded',
+                  },
+                },
+              },
+              loaded: {
+                type: 'final',
+              },
+            },
+          },
+          autoPlayingVideo: {},
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      reportVideoLoaded: assign({
+        hasVideoLoaded: true,
+      }),
+    },
+    guards: {
+      hasLoadedVideo: ctx => ctx.hasVideoLoaded,
+    },
+  }
+);
--- src/components/VideoCard/VideoCard.tsx
+++ src/components/VideoCard/VideoCard.tsx
@@ -1,47 +1,59 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { useMachine } from '@xstate/react';
+import { netflixStyleVideoHoverMachine } from './VideoCard.machine';
 
 const VideoCard: React.FC<{ title: Title }> = ({ title }) => {
-  const [canShowVideo, setCanShowVideo] = useState<boolean>(false);
+  const [state, send] = useMachine(netflixStyleVideoHoverMachine);
   const [lastStoppedTime, setLastStoppedTime] = useState<number>(0);
   const videoRef = useRef<HTMLVideoElement>(null);
 
   const handleMouseOver = async () => {
-    setCanShowVideo(true);
-    if (lastStoppedTime > 10) {
-      videoRef.current!.currentTime = 0;
-    }
-    const playPromise = videoRef.current?.play();
-    try {
-      await playPromise;
-    } catch (err) {}
+    send('MOUSE_OVER');
   };
 
-  const handleMouseOut = () => {
-    setCanShowVideo(false);
+  const handleMouseLeave = () => {
+    send('MOUSE_OUT');
+    videoRef.current!.pause();
     setLastStoppedTime(videoRef.current!.currentTime);
-    setLastStoppedTime(videoRef.current!.currentTime);
-    videoRef.current?.pause();
   };
+
+  const handleOnCanPlay = () => {
+    send('REPORT_VIDEO_LOADED');
+  };
+
+  useEffect(() => {
+    if (state.matches({ showingVideo: 'autoPlayingVideo' })) {
+      if (lastStoppedTime > 10) {
+        videoRef.current!.currentTime = 0;
+      }
+      videoRef.current!.play();
+    }
+  }, [state, lastStoppedTime]);
+
+  console.log(state.value);
 
   return (
     <div
       className='w-[350px] h-[200px]  rounded-md ml-3 flex-shrink-0 hover:scale-125 cursor-pointer  transition-transform'
       onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
+      onMouseLeave={handleMouseLeave}
     >
       <img
         src={title.cover}
         alt='cover'
-        className={`${canShowVideo ? 'hidden' : 'inline-block'} w-full h-full rounded-md`}
+        className={`${
+          state.matches({ showingVideo: 'autoPlayingVideo' }) ? 'hidden' : 'inline-block'
+        } w-full h-full rounded-md`}
       />
       <video
-        preload='none'
         playsInline
         ref={videoRef}
-        className={`${!canShowVideo ? 'hidden' : 'inline-block'} w-full h-full rounded-md `}
-      >
-        <source src={title.preview} type='video/mp4' />
-      </video>
+        onCanPlay={handleOnCanPlay}
+        className={`${
+          state.matches({ showingVideo: 'autoPlayingVideo' }) ? 'inline-block' : 'hidden'
+        } w-full h-full rounded-md `}
+        src={title.preview}
+      ></video>
     </div>
   );
 };
